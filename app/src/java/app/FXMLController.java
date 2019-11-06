85d52136408b3ddec7d28955c5915c39cc4077ca
fadfad <tuba13455@gmail.com>
2019-11-06 12:10:01 -0300

    Revert "CABALOGO"
    This reverts commit de727831046d66bdc54740017785b2ede70ec600.

--- app/src/java/app/FXMLController.java
+++ app/src/java/app/FXMLController.java
@@ -1,4 +1,4 @@
-package app.diario.telaloginadm;
+package app;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -6,15 +6,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class FXMLControllerTelaLoginAdm implements Initializable {
+public class FXMLController implements Initializable {
     
     @FXML
-//    private Label label;
+    private Label label;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-//        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
     }    
 }
