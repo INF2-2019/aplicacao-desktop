85e37e303845ba8147272bfa602dd12e0b72626c
pedrohenriquemop <madeirapedrohenrique1112@gmail.com>
2019-11-11 10:24:22 -0200

    Menu de relatórios

---
+++ app/src/java/app/diario/relatorios/src/principal/MainController.java
@@ -0,0 +1,57 @@
+package principal;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class MainController implements Initializable {
+
+	@FXML
+	private Button rel7Btn;
+	
+	@FXML
+	private Button rel8Btn;
+	
+	@FXML
+	private Button rel9Btn;
+	
+	@FXML
+	private Button rel10Btn;
+	
+	@FXML
+	private Button rel11Btn;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		System.out.println();
+	}
+
+	@FXML
+	private void rel7Action(ActionEvent event) {
+		
+	}
+
+	@FXML
+	private void rel8Action(ActionEvent event) {
+		
+	}
+
+	@FXML
+	private void rel9Action(ActionEvent event) {
+		
+	}
+
+	@FXML
+	private void rel10Action(ActionEvent event) {
+		
+	}
+
+	@FXML
+	private void rel11Action(ActionEvent event) {
+		
+	}
+	
+}
---
+++ app/src/java/app/diario/relatorios/src/principal/Relatorios.java
@@ -0,0 +1,28 @@
+package principal;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Relatorios extends Application {
+	
+	@Override
+	public void start(Stage stage) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+		
+		Scene sc = new Scene(root);
+		sc.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+		
+		stage.setTitle("Manutenção de Turmas");
+		stage.setScene(sc);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+}
