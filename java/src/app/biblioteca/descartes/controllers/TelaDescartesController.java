9aa1b16967875e28b2c417889f72a0e8cd931a0a
Marcus <marcusarcanjo02@gmail.com>
2019-11-14 10:13:27 -0300

    Organizando
    Ainda com erro de inicizializar a tela

---
+++ java/src/app/biblioteca/descartes/controllers/TelaDescartesController.java
@@ -0,0 +1,88 @@
+package app.biblioteca.descartes.controllers;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import java.sql.Date;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import app.DescartadoLista;
+import app.DescartadoLista;
+import app.Funções;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.stage.Stage;
+/**
+ *
+ * @author User
+ */
+public class TelaDescartesController implements Initializable {
+  
+   
+   
+    @FXML
+    private Label label;
+    @FXML
+    private Button Descartar;
+    @FXML
+    private TableView<DescartadoLista> TabelaDescarte;
+
+    @FXML
+    private TableColumn<DescartadoLista ,String> ACERVO;
+   
+    @FXML
+   private TableColumn<DescartadoLista,String> OPERADOR;
+    @FXML
+    private TableColumn<DescartadoLista, String > DATA;
+    @FXML
+    private TableColumn<DescartadoLista,String> MOTIVO;
+
+   List<DescartadoLista> ListaDescartados;
+private ObservableList<DescartadoLista> a =FXCollections.observableArrayList();
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    
+    List ListaD = null;
+      Funções fun =new Funções();
+        try {
+             ListaD =fun.ExibeOqueJaFoiDescartado();
+        } catch (SQLException ex) {
+            Logger.getLogger(TelaDescartesController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+
+        ACERVO.setCellValueFactory(new PropertyValueFactory<>("ACERVO"));
+        MOTIVO.setCellValueFactory(new PropertyValueFactory<>("MOTIVO"));
+        OPERADOR.setCellValueFactory(new PropertyValueFactory<>("OPERADOR"));
+        DATA.setCellValueFactory(new PropertyValueFactory<>("DATA"));
+    
+        TabelaDescarte.setItems(FXCollections.observableArrayList(ListaD));
+       TabelaDescarte.getColumns().addAll(ACERVO,MOTIVO,OPERADOR,DATA);
+        
+       
+    
+      
+    }
+    
+    
+    }
+
+
+    
