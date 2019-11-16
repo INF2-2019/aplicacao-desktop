f65810c258370538055fccac3ce99af5eb6fcab3
IndraMatsiendra <56325439+IndraMatsiendra@users.noreply.github.com>
2019-11-16 15:32:19 -0300

    inserir em 'academicos'

---
+++ src/java/app/biblioteca/acervo/inserir/academico/InsereAcademicoMain.java
@@ -0,0 +1,43 @@
+package app.biblioteca.acervo.inserir.academico;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Indra Matsiendra & Amanda Jacomette
+ */
+public class InsereAcademicoMain extends Application {
+    public static Stage stage;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/app/biblioteca/acervo/inserir/academico/InsereAcademicoFXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+        setStage(stage);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public static Stage getStage(){
+        return stage;
+    }
+
+    private void setStage(Stage stage) {
+        InsereAcademicoMain.stage = stage;
+    }
+}
