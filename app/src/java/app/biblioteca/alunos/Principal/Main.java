630bd5be951def0f26eef69dada2013c509cae55
Osmar Costa Alves Coelho <osmarcoelho25.oc@gmail.com>
2019-11-18 12:41:36 -0200

    Consertamos os erros nos relatorios, adicionamos a parte campi/aluno da biblioteca

---
+++ app/src/java/app/biblioteca/alunos/Principal/Main.java
@@ -0,0 +1,34 @@
+
+package app.biblioteca.alunos.Principal;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    private static Stage stage;
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/app/biblioteca/alunos/principal/FXML2.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setResizable(false);
+        stage.setScene(scene);
+               stage.setWidth(1280);
+        stage.show();
+        setStage(stage);
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+      public static Stage getStage(){
+        return stage;
+    }
+
+    private void setStage(Stage stage) {
+        this.stage = stage;
+    }
+}
---
+++ app/src/java/app/biblioteca/campi/Alterar/AlteraController.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.biblioteca.campi.Alterar;
+
+
+import app.biblioteca.campi.Principal.DbConnector;
+import app.biblioteca.campi.Principal.TableController;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Marcelo
+ */
+public class AlteraController implements Initializable {
+    static int id;
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+	AlteraController.id = id;
+    }
+    
+   
+   @FXML
+    private TextField nomeInput;
+
+    @FXML
+    private Button botaoCancelar;
+
+    @FXML
+    private Label AnoLabel1;
+
+    @FXML
+    private Button botaoSalvar;
+
+    @FXML
+    private TextField ufInput;
+
+    @FXML
+    private TextField cityInput;
+
+    @FXML
+    private Label AnoLabel4;
+
+    @FXML
+    private Label AnoLabel2;
+
+
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       
+    }
+    
+    @FXML
+    void AcaoCancelar(ActionEvent event) {
+	fechar();
+    }
+
+    private void fechar() {
+	AlteraMain.getStage().close();
+    }
+    
+    @FXML
+    void Alterar(ActionEvent event) {
+     
+	try {
+            Connection connection = DbConnector.getConnection();
+	   
+            if(!"".equals(nomeInput.getText()))
+                    {
+                      PreparedStatement stmt = connection.prepareStatement("UPDATE `campi` SET"
+                    + " `nome` = '"+nomeInput.getText()+"'"
+                    
+                    + " WHERE `campi`.`id` = "+AlteraController.getId());
+            stmt.execute();
+            stmt.close();  
+                    }
+            
+                 if(!"".equals(cityInput.getText()))
+                    {
+                      PreparedStatement stmt = connection.prepareStatement("UPDATE `campi` SET"
+                    + " `cidade` = '"+cityInput.getText()+"'"
+                    
+                    + " WHERE `campi`.`id` = "+AlteraController.getId());
+            stmt.execute();
+            stmt.close();  
+                    }
+                   
+	      if(!"".equals(ufInput.getText()))
+                    {
+                      PreparedStatement stmt = connection.prepareStatement("UPDATE `campi` SET"
+                    + " `uf` = '"+ufInput.getText()+"'"
+                    
+                    + " WHERE `campi`.`id` = "+AlteraController.getId());
+            stmt.execute();
+            stmt.close();  
+                    }
+             
+                
+	    
+            connection.close();
+            fechar();
+        } catch (SQLException ex) {
+            Logger.getLogger(TableController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    
+        
+    }
+    
+}
